cdf7c913d3ccd02f76e68a7b91b28340349e26f5
kelvincreaciones <kelvincreaciones@gmail.com>
2026-07-03 14:59:31 -0400

    Actualizar menú desde el panel de admin

--- config.js
+++ config.js
@@ -57,11 +57,11 @@ const CONFIG = {
       "turnos": [
         {
           "abre": "06:00",
-          "cierra": "22:30"
+          "cierra": "11:30"
         },
         {
-          "abre": "11:30",
-          "cierra": "16:00"
+          "abre": "15:00",
+          "cierra": "22:00"
         }
       ]
     },
